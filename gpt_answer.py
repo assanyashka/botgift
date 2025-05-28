74de49c37c8a65cb9b8c3561484c1f58f61925e3
assanyashka <assav68@gmail.com>
2025-05-28 12:13:07 +0600

    Update gpt_answer.py

--- gpt_answer.py
+++ gpt_answer.py
@@ -1,20 +1,15 @@
 from g4f.client import Client
 import config
 
-prompt = config.GPT_PROMPT
-
 def generate(query):
     try:
         client = Client()
         response = client.chat.completions.create(
-            model="gpt-4o-mini",
-            messages=[{"role": "user", "content": f"{prompt + query}"}],
+            model="gpt-4-turbo",
+            messages=[{"role": "user", "content": f"{config.GPT_PROMPT + query}"}],
             web_search=False
         )
-        response_text = response.choices[0].message.content
-
-        return response_text
-
+        return response.choices[0].message.content
     except Exception as e:
         print(f"Ошибка при обработке текста через g4f: {e}")
         return None
